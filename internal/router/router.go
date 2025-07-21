c0c86ce2120ee991377bd8425402c2f0b2c25384
GadXx <dd94307@gmail.com>
2025-07-21 17:41:01 +0300

    Fix router

--- internal/router/router.go
+++ internal/router/router.go
@@ -15,18 +15,17 @@ import (
 func NewRouter(authHandler *handler.AuthHandler, blackList *service.BlacklistService) *chi.Mux {
 	router := chi.NewRouter()
 
-	router.Use(chiMiddleware.Recoverer)
 	router.Use(chiMiddleware.Logger)
 
 	router.Get("/docs/*", httpSwagger.WrapHandler)
 
 	router.Get("/new_session/{user_id}", authHandler.HandleNewSession)
+	router.Get("/refresh", authHandler.RefreshSession)
+	router.Post("/refresh/revoke", authHandler.RevokeSession)
 
 	router.Group(func(r chi.Router) {
 		r.Use(middleware.AuthMiddleware(blackList))
 		r.Get("/me", authHandler.GetAuthenticatedUserID)
-		r.Get("/refresh", authHandler.RefreshSession)
-		r.Post("/refresh/revoke", authHandler.RevokeSession)
 	})
 
 	return router
